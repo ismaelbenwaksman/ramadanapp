5e9e4efbd3f560b8fe9dac40bc30fc6f1fad3c3f
ismaelbenwaksman <ismaelbenwaksman@gmail.com>
2026-02-18 21:24:22 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+// Clean up: unregister and clear caches
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
+  );
+  self.registration.unregister();
+});
